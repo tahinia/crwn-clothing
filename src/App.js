22e71c8819815868f351c41f3f2e8c290e505c88
Taghinia <keivan.taghinia@gmail.com>
2021-12-13 14:58:04 +0000

    updating app with shop, collection preview and item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
